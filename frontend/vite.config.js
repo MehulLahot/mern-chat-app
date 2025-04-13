e62ea1c2a2ebbc1ec08c26054a629154ae2b436b
Mehul <mehullahot07@gmail.com>
2025-04-13 23:42:47 +0530

    add new dependencies

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// // import tailwindcss from "@tailwindcss/vite";  // No longer needed
+// import tailwindcss from "tailwindcss"; // Correct usage for PostCSS
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [
+//         tailwindcss(), // Add tailwindcss here as a PostCSS plugin
+//       ],
+//     },
+//   },
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5000",
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "tailwindcss";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
