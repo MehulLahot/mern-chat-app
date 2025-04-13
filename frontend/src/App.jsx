9e23698e770f90d7bd0547470546a8dfe1bf92cb
Mehul <mehullahot07@gmail.com>
2025-04-14 00:37:30 +0530

    attempt- 101

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/Signup";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
-import SignUp from "./pages/signup/Signup";
 
 function App() {
   const { authUser } = useAuthContext();
